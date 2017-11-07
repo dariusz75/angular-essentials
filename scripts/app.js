05e82c69150a850513966da8df192117bbd86c9d
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-11-07 16:25:27 +0000

    setup wiev added

--- scripts/app.js
+++ scripts/app.js
@@ -13,6 +13,11 @@ var myApp = angular
 									templateUrl: 'templates/nested.html',
 									controller: 'nestedController'
 								})
+								.state('setup', {
+									url: '/setup',
+									templateUrl: 'templates/setup.html',
+									controller: 'setupController'
+								})
 
 								//$urlRouterProvider.otherwise('/home');
 						})
@@ -22,13 +27,16 @@ var myApp = angular
 						.controller('nestedController', function($scope) {
 							$scope.message = "Message from Nested Controller";
 						})
+						.controller('setupController', function($scope) {
+							$scope.message = "Message from Setup Controller";
+						})
 						.controller('sidebarController', function($scope) {
 							var chapters = [
-															{chapterName: 'Setup'},
-															{chapterName: 'Module'},
-															{chapterName: 'Controller'},
-															{chapterName: 'src directive'},
-															{chapterName: 'Two-way data binding'},
+															{chapterName: 'Setup', url: 'setup'},
+															{chapterName: 'Module', url: '#'},
+															{chapterName: 'Controller', url: '#'},
+															{chapterName: 'src directive', url: '#'},
+															{chapterName: 'Two-way data binding', url: '#'},
 															];
 
 							$scope.chapters = chapters;

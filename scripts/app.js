6f5c33330cd9d1d67823fa865106015c6baaadf9
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-11-08 12:03:16 +0000

    templates added

--- scripts/app.js
+++ scripts/app.js
@@ -28,6 +28,70 @@ var myApp = angular
 									templateUrl: 'templates/controller.html',
 									controller: 'controllerController'
 								})
+								.state('src-directive', {
+									url: '/src-directive',
+									templateUrl: 'templates/src-directive.html'
+								})
+								.state('two-way-databinding', {
+									url: '/two-way-databinding',
+									templateUrl: 'templates/two-way-databinding.html'
+								})
+								.state('ng-repeat-directive', {
+									url: '/ng-repeat-directive',
+									templateUrl: 'templates/ng-repeat-directive.html'
+								})
+								.state('events-handling', {
+									url: '/events-handling',
+									templateUrl: 'templates/events-handling.html'
+								})
+								.state('filters', {
+									url: '/filters',
+									templateUrl: 'templates/filters.html'
+								})
+								.state('data-sorting', {
+									url: '/data-sorting',
+									templateUrl: 'templates/data-sorting.html'
+								})
+								.state('data-sorting-by-search-filter', {
+									url: '/data-sorting-by-search-filter',
+									templateUrl: 'templates/data-sorting-by-search-filter.html'
+								})
+								.state('data-sorting-by-multiple-properties', {
+									url: '/data-sorting-by-multiple-properties',
+									templateUrl: 'templates/data-sorting-by-multiple-properties.html'
+								})
+								.state('ng-hide-and-ng-show-directives', {
+									url: '/ng-hide-and-ng-show-directives',
+									templateUrl: 'templates/ng-hide-and-ng-show-directives.html'
+								})
+								.state('ng-init-directive', {
+									url: '/ng-init-directive',
+									templateUrl: 'templates/ng-init-directive.html'
+								})
+								.state('ng-include-directive', {
+									url: '/ng-include-directive',
+									templateUrl: 'templates/ng-include-directive.html'
+								})
+								.state('services', {
+									url: '/services',
+									templateUrl: 'templates/services.html'
+								})
+								.state('services-separated', {
+									url: '/services-separated',
+									templateUrl: 'templates/services-separated.html'
+								})
+								.state('anchorscroll-service', {
+									url: '/anchorscroll-service',
+									templateUrl: 'templates/anchorscroll-service.html'
+								})
+								.state('routing', {
+									url: '/routing',
+									templateUrl: 'templates/routing.html'
+								})
+								.state('nested-routing', {
+									url: '/nested-routing',
+									templateUrl: 'templates/nested-routing.html'
+								})
 
 								//$urlRouterProvider.otherwise('/home');
 						})
@@ -51,8 +115,22 @@ var myApp = angular
 															{chapterName: 'Setup', url: 'setup'},
 															{chapterName: 'Module', url: 'module'},
 															{chapterName: 'Controller', url: 'controller'},
-															{chapterName: 'src directive', url: '#'},
-															{chapterName: 'Two-way data binding', url: '#'},
+															{chapterName: 'src directive', url: 'src-directive'},
+															{chapterName: 'Two-way data binding', url: 'two-way-databinding'},
+															{chapterName: 'ng-repeat-directive', url: 'ng-repeat-directive'},
+															{chapterName: 'Events Handling', url: 'events-handling'},
+															{chapterName: 'Filters', url: 'filters'},
+															{chapterName: 'Data Sorting', url: 'data-sorting'},
+															{chapterName: 'Data sorting by search filter', url: 'data-sorting-by-search-filter'},
+															{chapterName: 'Data sorting by multiple properties', url: 'data-sorting-by-multiple-properties'},
+															{chapterName: 'ng-hide and ng-show directives', url: 'ng-hide-and-ng-show-directives'},
+															{chapterName: 'ng init directive', url: 'ng-init-directive'},
+															{chapterName: 'ng-include-directive', url: 'ng-include-directive'},
+															{chapterName: 'Services', url: 'services'},
+															{chapterName: 'Services separated', url: 'services-separated'},
+															{chapterName: 'Anchorscroll service', url: 'anchorscroll-service'},
+															{chapterName: 'Routing', url: 'routing'},
+															{chapterName: 'Nested routing', url: 'nested-routing'}
 															];
 
 							$scope.chapters = chapters;

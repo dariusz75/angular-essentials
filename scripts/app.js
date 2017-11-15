a6c040a3168d8ea423d82905e030bd826d2a49ac
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-11-15 14:32:44 +0000

    filters page finished

--- scripts/app.js
+++ scripts/app.js
@@ -49,7 +49,8 @@ var myApp = angular
 								})
 								.state('filters', {
 									url: '/filters',
-									templateUrl: 'templates/filters.html'
+									templateUrl: 'templates/filters.html',
+									controller: 'filtersController'
 								})
 								.state('data-sorting', {
 									url: '/data-sorting',
@@ -195,6 +196,61 @@ var myApp = angular
 																					$scope.incrementDislikes = function (technology) {
 																						technology.dislikes++
 																					};
+						})
+						.controller('filtersController', function($scope) {
+							var employees = [
+													{
+														name: 'Ben',
+														dateOfBirth: new Date('November 23, 1999'), 
+														gender: 'Male',
+														salary: 22222.959696,
+													},
+													{
+														name: 'Claire',
+														dateOfBirth: new Date('November 13, 1989'), 
+														gender: 'Female',
+														salary: 33333.76777,
+													},
+													{
+														name: 'Josh',
+														dateOfBirth: new Date('November 23, 1993'), 
+														gender: 'Male',
+														salary: 44444.6666,
+													},
+													{
+														name: 'Benny',
+														dateOfBirth: new Date('November 13, 2000'), 
+														gender: 'Male',
+														salary: 55555.099,
+													},
+													{
+														name: 'John',
+														dateOfBirth: new Date('November 23, 1999'), 
+														gender: 'Male',
+														salary: 66662.959696,
+													},
+													{
+														name: 'Andrew',
+														dateOfBirth: new Date('November 13, 1989'), 
+														gender: 'Male',
+														salary: 77777.76777,
+													},
+													{
+														name: 'Josh',
+														dateOfBirth: new Date('November 23, 1993'), 
+														gender: 'Male',
+														salary: 88888.6666,
+													},
+													{
+														name: 'Betty',
+														dateOfBirth: new Date('November 13, 2000'), 
+														gender: 'Female',
+														salary: 99999.099,
+													},
+												];
+
+										$scope.employees = employees;
+										$scope.rowLimit = 2;
 						})
 						.controller('sidebarController', function($scope) {
 							var chapters = [

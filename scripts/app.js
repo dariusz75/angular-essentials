b7daae71434d22b57ecb611f740a42852a45bcca
TCSL\DariuszF <dariusz.franczak@taxcomputersystems.com>
2017-11-15 15:14:15 +0000

    data sorting page finished

--- scripts/app.js
+++ scripts/app.js
@@ -54,7 +54,8 @@ var myApp = angular
 								})
 								.state('data-sorting', {
 									url: '/data-sorting',
-									templateUrl: 'templates/data-sorting.html'
+									templateUrl: 'templates/data-sorting.html',
+									controller: 'dataSortingController'
 								})
 								.state('data-sorting-by-search-filter', {
 									url: '/data-sorting-by-search-filter',
@@ -251,6 +252,44 @@ var myApp = angular
 
 										$scope.employees = employees;
 										$scope.rowLimit = 2;
+						})
+						.controller('dataSortingController', function($scope) {
+							var employees = [
+																{
+																	name: 'Ben',
+																	dateOfBirth: new Date('November 23, 1999'), 
+																	gender: 'Male',
+																	salary: 22222.959696,
+																},
+																{
+																	name: 'Bill',
+																	dateOfBirth: new Date('September 13, 1989'), 
+																	gender: 'Male',
+																	salary: 33333.76777,
+																},
+																{
+																	name: 'Josh',
+																	dateOfBirth: new Date('June 23, 1993'), 
+																	gender: 'Male',
+																	salary: 44444.6666,
+																},
+																{
+																	name: 'Andrew',
+																	dateOfBirth: new Date('January 13, 2000'), 
+																	gender: 'Male',
+																	salary: 55555.099,
+																},
+																{
+																	name: 'John',
+																	dateOfBirth: new Date('March 13, 2000'), 
+																	gender: 'Male',
+																	salary: 66666.999,
+																},
+															];
+
+										$scope.employees = employees;
+										$scope.sortColumn = 'name';
+
 						})
 						.controller('sidebarController', function($scope) {
 							var chapters = [
